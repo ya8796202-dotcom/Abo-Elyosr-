7b9d8c3544bf6f6ab582e977c14d33e057f363e5
ya8796202-dotcom <ya8796202@gmail.com>
2025-11-12 12:15:05 +0200

    Update script.js
    Signed-off-by: ya8796202-dotcom <ya8796202@gmail.com>

--- script.js
+++ script.js
@@ -128,3 +128,31 @@
     sbtBtn.addEventListener("click", sbtNewTip);
   });
 </script>
+
+
+
+
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("arduino-cache").then(cache => {
+      return cache.addAll([
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
